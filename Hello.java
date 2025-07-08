affab31d5e374de6cebf0450e9ba2c41e29ce534
SangJae Lee <lsj020618@gmail.com>
2025-07-08 16:29:25 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,30 @@
+package mymain;
+
+public class Hello {
+
+//	단축키 
+//	1라인 주석     : ctrl + /(toggle)
+//	여러라인주석   : ctrl + shift + / (설정)
+//			   	   : ctrl + shift + \ (해제)
+//	화면확대/축소  : ctrl + shift + (+)
+//	화면확대/축소  : ctrl + shift + (-)
+//	화면정렬       : ctrl + shift + F
+
+	public static void main(String[] args) {
+		/*
+		 * System : 클래스명 out : 객체명 println : 메소드명(함수명)
+		 * 
+		 * CUI(Console User Interface) 
+		 * or 
+		 * TUI(Text    User Interface)
+		 * 
+		 * 
+		 */
+		// TODO Auto-generated method stub
+		System.out.println("마 반갑다");
+		System.out.println("마 니 고향이 어대고");
+		System.out.println("부산 북구? 내도 북구다!");
+		System.out.println("니 그집 아들이가 마 내 거기 단골이다!");
+	}
+
+}
