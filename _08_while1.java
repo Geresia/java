b40df4a9f9acfe5d62e2bfdeba3e2157ac5f0723
SangJae Lee <lsj020618@gmail.com>
2025-07-15 18:15:47 +0900

    Add files via upload

---
+++ _08_while1.java
@@ -0,0 +1,14 @@
+package mymain;
+
+public class _08_while1 {
+	public static void main(String[] args) {
+		
+		int n = 0;
+		while(n<3){
+			System.out.println("니하오마");
+			n++;
+		}
+		
+		System.out.printf("%d<3 : %b", n, n<3 );
+	}
+}
