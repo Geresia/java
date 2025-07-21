ee3edaa35a46291a0c9f9a95bc4fa94a1c2d64f8
SangJae Lee <lsj020618@gmail.com>
2025-07-21 18:01:54 +0900

    Add files via upload

---
+++ myutil/MyDate4.java
@@ -0,0 +1,38 @@
+package myutil;
+
+public class MyDate4 {
+	
+	int year;
+	int month;
+	int day;
+	
+	// Alt + Shift + S
+	
+	public MyDate4() {
+//		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyDate4(int year, int month, int day) {
+//		super();
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
+
+	public MyDate4(int year) {
+		super();
+		this.year = year;
+	}
+
+	public MyDate4(int year, int month) {
+		super();
+		this.year = year;
+		this.month = month;
+	}
+	
+	
+	
+	
+	
+}
---
+++ myutil/MyTime.java
@@ -0,0 +1,51 @@
+package myutil;
+
+public class MyTime {
+	int hour;
+	int minute;
+	int second;
+	
+	public MyTime() {
+		
+	}
+	
+	public MyTime(int h, int m, int s) {
+		this.hour = h;
+		this.minute = m;
+		this.second = s;
+	}
+	public MyTime(int h, int m) {
+		this.hour = h;
+		this.minute = m;
+	}
+	public MyTime(int h) {
+		this.hour = h;
+	}
+	public void display1() {
+		System.out.printf("%02d:%02d:%02d\n",
+				hour,minute,second
+	);
+	}
+	public void display2() {
+		System.out.printf("%02d시%02d분%02d초\n",
+			hour,minute,second
+	);
+	}
+	public void display3() {
+		String AmPm;
+		if(hour >= 0 && hour < 12) {
+			AmPm = "오전";
+		}else if(hour == 12) {
+			AmPm = "오후";
+		}else if(hour > 12){
+			hour = hour - 12;
+			AmPm = "오후";
+		}else{
+			System.out.println("시간은 음수가 없습니다");
+			return;
+		}
+		System.out.printf("%s %02d:%02d:%02d\n",
+				AmPm, hour,minute,second
+		);
+}
+}
